d6ac9bfd23b3bc5ef24e686643627663185927f8
Shubham Mishra1 <shubham_mishra1@epam.com>
2023-06-11 21:57:52 +0530

    main commit

---
+++ practice/src/com/shubham/practice/Main.java
@@ -0,0 +1,11 @@
+package com.shubham.practice;
+
+public class Main {
+    public static void main(String[] args) {
+        function1();
+    }
+
+    public static void function1() {
+        System.out.println("function1");
+    }
+}
