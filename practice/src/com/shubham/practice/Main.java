1422eb455be8228e6aad499afcccdf0c66fcb788
Shubham Mishra1 <shubham_mishra1@epam.com>
2023-06-11 22:00:06 +0530

    feature1

--- practice/src/com/shubham/practice/Main.java
+++ practice/src/com/shubham/practice/Main.java
@@ -3,9 +3,14 @@ package com.shubham.practice;
 public class Main {
     public static void main(String[] args) {
         function1();
+        function3();
     }
 
     public static void function1() {
         System.out.println("function1");
+    }
+
+    public static void function3() {
+        System.out.println("function3");
     }
 }
